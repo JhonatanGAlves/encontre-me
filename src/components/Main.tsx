137555fa0812cb75d398e18990f923f7b77068e1
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-02-06 13:01:52 -0300

    Completed Main component banner section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import BgBanner from '../assets/images/background-main-banner.png'
 import ImgBanner from '../assets/images/isac-manoel-domingues.png'
-import Button from './Button'
 
 export const Main = () => {
   return (
@@ -11,30 +10,28 @@ export const Main = () => {
         <div className='content-banner'>
           <div className='txt-buttons-banner'>
             <h1>registre seu caso ou seja<br />
-              mais um <style color='#7393C0'>anjo</style> espalhado<br />
+              mais um <span style={{ color: '#7393C0' }}>anjo</span> espalhado<br />
               pelo mundo!
             </h1>
-            <Buttons>
-              <Button
-                value='ser um anjo'
-                width='200px'
-                bgColor='transparent'
-                color='#7393C0'
-                border='2px solid #7393C0'
-              />
-              <Button
-                value='novo caso'
-                width='200px'
-                bgColor='transparent'
-                color='#7393C0'
-                border='2px solid #7393C0'
-              />
+            <p>Ao se cadastrar como um <strong>anjo</strong>, quando alguém entrar com um <strong>novo caso</strong>,<br />
+              você receberá automaticamente todos os dados do desaparecido,<br />
+              nome completo, foto, idade, data, local do desaparecimento e<br />
+              alguma observação relevante.<br /><br />
 
+              Clique em <strong>SER UM ANJO</strong> ou, se tiver algum caso, clique em <strong>NOVO CASO</strong>.
+            </p>
+            <Buttons>
+              <a href='#'>
+                <button className='anjo-button'>ser um anjo</button>
+              </a>
+              <a href='#'>
+                <button className='caso-button'>novo caso</button>
+              </a>
             </Buttons>
           </div>
         </div>
       </div>
-    </StyledMain>
+    </StyledMain >
   )
 }
 
@@ -54,12 +51,13 @@ const StyledMain = styled.div`
     overflow: hidden;
     width: 100%;
     height: auto;
+    margin-top: -60px;
     
     .content-banner {
       margin: auto;
       width: 1450px;
       max-width: 1450px;
-      padding: 200px 0;
+      padding: 260px 0 100px 0;
 
       .txt-buttons-banner {
         display: flex;
@@ -69,6 +67,15 @@ const StyledMain = styled.div`
         h1 {
           font-size: 48px;
           text-transform: uppercase;
+
+          color: #FFF;
+        }
+
+        p {
+          font-size: 21px;
+          font-weight: 300;
+
+          color: #FFF;
         }
       }
     }
@@ -76,4 +83,28 @@ const StyledMain = styled.div`
 `
 const Buttons = styled.div`
   display: flex;
+
+  a {
+    margin: 20px 20px 45px 0;
+
+    button {
+      width: 300px;
+      height: 50px;
+      padding: 5px 10px;
+      border-radius: 10px;
+      border: none;
+      font-size: 21px;
+      font-weight: 600;
+      text-transform: uppercase;
+      cursor: pointer;
+
+      color: #FFF;
+      background-color: #7393C0;
+
+      &:hover {
+        background-color: #78a5e6;
+        color: #FFF;
+      }
+    }
+  }
 `
