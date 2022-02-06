263b04c4b149bd3c9052f3a598583534dc6a3816
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-02-06 00:38:01 -0300

    import components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+import Header from './components/Header'
+import Main from './components/Main'
 
 export const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <StyledApp className="App">
+      <Header />
+      <Main />
+    </StyledApp>
   )
 }
 
 export default App
+
+/*---------STYLED COMPONENTS---------- */
+const StyledApp = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+
+  color: #CCC;
+`
